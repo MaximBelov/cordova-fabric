3fd86f3f916697536cd24f41fea18b6c0e31ccee
Maxim Belov <belov1988@gmail.com>
2019-08-09 16:59:10 +0300

    "crashlytics": "3.13.4"

--- scripts/add-packages.sh
+++ scripts/add-packages.sh
@@ -7,35 +7,38 @@
 #
 # Requires `jq` (OSX: `brew install jq`)
 
-FABRIC_VERSION=$(cat package.json | jq -r '.packages.fabric')
-CRASH_VERSION=$(cat package.json | jq -r '.packages.crashlytics')
+#FABRIC_VERSION=$(cat package.json | jq -r '.packages.fabric')
+#CRASH_VERSION=$(cat package.json | jq -r '.packages.crashlytics')
 
-echo "Adding packages from package.json:"
-echo "Fabric version $FABRIC_VERSION"
-echo "Crashlytics version $CRASH_VERSION"
+#echo "Adding packages from package.json:"
+#echo "Fabric version $FABRIC_VERSION"
+#echo "Crashlytics version $CRASH_VERSION"
 
 # Create ./tmp/ and download packages
-echo "Creating ./tmp/, downloading packages..."
-rm -rf ./tmp/
-mkdir -p ./tmp/
-curl -o ./tmp/fabric.zip "https://kit-downloads.fabric.io/cocoapods/fabric/$FABRIC_VERSION/fabric.zip"
-curl -o ./tmp/crashlytics.zip "https://kit-downloads.fabric.io/cocoapods/crashlytics/$CRASH_VERSION/crashlytics.zip"
+#echo "Creating ./tmp/, downloading packages..."
+#rm -rf ./tmp/
+#mkdir -p ./tmp/
+#curl -o ./tmp/fabric.zip "https://kit-downloads.fabric.io/cocoapods/fabric/$FABRIC_VERSION/fabric.zip"
+#curl -o ./tmp/fabric.zip "https://kit-downloads.fabric.io/cocoapods/fabric/1.9.0/fabric.zip"
+#curl -o ./tmp/crashlytics.zip "https://kit-downloads.fabric.io/cocoapods/crashlytics/$CRASH_VERSION/crashlytics.zip"
+#curl -o ./tmp/crashlytics.zip "https://kit-downloads.fabric.io/cocoapods/crashlytics/3.13.4/crashlytics.zip"
 
 # Unzip targets
-echo "Extracting frameworks..."
-unzip -qq ./tmp/fabric.zip -d ./tmp/fabric
-unzip -qq ./tmp/crashlytics.zip -d ./tmp/crashlytics
+#echo "Extracting frameworks..."
+#unzip -qq ./tmp/fabric.zip -d ./tmp/fabric
+#unzip -qq ./tmp/crashlytics.zip -d ./tmp/crashlytics
 
 # Clean destination
-echo "Cleaning destination ./lib/ios/ ..."
-rm -rf ./lib/ios/Crashlytics.framework ./lib/ios/Fabric.framework
+#echo "Cleaning destination ./lib/ios/ ..."
+#rm -rf ./lib/ios/Crashlytics.framework ./lib/ios/Fabric.framework
 
 # Move files
-echo "Transferring frameworks to ./lib/ios/ ..."
-mv ./tmp/crashlytics/iOS/Crashlytics.framework ./tmp/fabric/iOS/Fabric.framework ./lib/ios/
+#echo "Transferring frameworks to ./lib/ios/ ..."
+#mv ./tmp/crashlytics/iOS/Crashlytics.framework ./lib/ios/
+#mv ./tmp/fabric/iOS/Fabric.framework ./lib/ios/
 
 # Cleanup
-echo "Cleaning temp directory..."
-rm -rf ./tmp/
+#echo "Cleaning temp directory..."
+#rm -rf ./tmp/
 
-echo "Package install complete!"
+#echo "Package install complete!"
